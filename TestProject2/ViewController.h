2d94273201b9a3cd2fea1725ba5bd989aff0f203
cpbarry@gmail.com <cpbarry@gmail.com>
2015-04-18 12:13:35 -0700

    Initial Commit

---
+++ TestProject2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestProject2
+//
+//  Created by Colin Barry on 4/18/15.
+//  Copyright (c) 2015 Arizona State University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestProject2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestProject2
+//
+//  Created by Colin Barry on 4/18/15.
+//  Copyright (c) 2015 Arizona State University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
